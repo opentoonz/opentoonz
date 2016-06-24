1710a06a4ae4cafee649d6cfb054ce2bff30fcca
Xelloss <xelloss@vip.qq.com>
2016-06-24 11:51:20 +0800

    remove modifications
    Check failed

--- toonz/sources/tnztools/stylepickertool.cpp
+++ toonz/sources/tnztools/stylepickertool.cpp
@@ -1,244 +1,254 @@
 
 
-#include "tools/tool.h"
-#include "tools/cursors.h"
 #include "tools/stylepicker.h"
-#include "toonz/txshsimplelevel.h"
-#include "toonz/txshlevelhandle.h"
-
-#include "drawutil.h"
+#include "tcolorstyles.h"
+#include "tstroke.h"
+#include "tofflinegl.h"
+#include "tvectorrenderdata.h"
 #include "tvectorimage.h"
 #include "ttoonzimage.h"
-#include "tundo.h"
-
-#include "toonz/tpalettehandle.h"
-
-#include "toonz/stage2.h"
-#include "tproperty.h"
-#include "toonzqt/tselectionhandle.h"
-#include "toonzqt/styleselection.h"
-#include "tools/toolhandle.h"
-#include "toonz/tframehandle.h"
-#include "toonz/txsheethandle.h"
-#include "toonz/preferences.h"
-#include "toonz/tcolumnhandle.h"
+#include "trasterimage.h"
 #include "toonz/dpiscale.h"
-#include "toonz/palettecontroller.h"
-#include <QCoreApplication>  // For Qt translation support
+#include "tpixelutils.h"
+#include "tregion.h"
 
-#define LINES L"Lines"
-#define AREAS L"Areas"
-#define ALL L"Lines & Areas"
+//---------------------------------------------------------
+
+StylePicker::StylePicker(const TImageP &image)
+    : m_image(image), m_palette(image->getPalette()) {}
+
+//---------------------------------------------------------
+
+StylePicker::StylePicker(const TImageP &image, const TPaletteP &palette)
+    : m_image(image), m_palette(palette) {}
+
+//---------------------------------------------------------
+
+TPoint StylePicker::getRasterPoint(const TPointD &p) const {
+  if (TToonzImageP ti = m_image) {
+    // DpiScale dpiScale(ti);
+    TDimension size = ti->getSize();
+    return TPoint(tround(0.5 * size.lx + p.x),   /// dpiScale.getSx()),
+                  tround(0.5 * size.ly + p.y));  /// dpiScale.getSy()));
+  } else if (TRasterImageP ri = m_image) {
+    // DpiScale dpiScale(ri);
+    TDimension size = ri->getRaster()->getSize();
+    return TPoint(tround(0.5 * size.lx + p.x),   // /dpiScale.getSx()),
+                  tround(0.5 * size.ly + p.y));  // /dpiScale.getSy()));
+  } else
+    return TPoint(tround(p.x), tround(p.y));
+}
+
+//---------------------------------------------------------
+/*-- (StylePickerTool内で)LineとAreaを切り替えてPickできる。mode: 0=Area,
+ * 1=Line, 2=Line&Areas(default)  --*/
+int StylePicker::pickStyleId(const TPointD &pos, double radius2,
+                             int mode) const {
+  int styleId = 0;
+  if (TToonzImageP ti = m_image) {
+    TRasterCM32P ras = ti->getRaster();
+    TPoint point     = getRasterPoint(pos);
+    if (!ras->getBounds().contains(point)) return -1;
+    TPixelCM32 col = ras->pixels(point.y)[point.x];
+
+    switch (mode) {
+    case 0:  // AREAS
+      styleId = col.getPaint();
+      break;
+    case 1:  // LINES
+      styleId = col.getInk();
+      break;
+    case 2:  // ALL (Line & Area)
+    default:
+      styleId = col.isPurePaint() ? col.getPaint() : col.getInk();
+      break;
+    }
+  } else if (TRasterImageP ri = m_image) {
+    const TPalette *palette = m_palette.getPointer();
+    if (!palette) return -1;
+    TRaster32P ras = ri->getRaster();
+    if (!ras) return -1;
+    TPoint point = getRasterPoint(pos);
+    if (!ras->getBounds().contains(point)) return -1;
+    TPixel32 col = ras->pixels(point.y)[point.x];
+    styleId      = palette->getClosestStyle(col);
+  } else if (TVectorImageP vi = m_image) {
+    // prima cerca lo stile della regione piu' vicina
+    TRegion *r     = vi->getRegion(pos);
+    if (r) styleId = r->getStyle();
+    // poi cerca quello della stroke, ma se prima aveva trovato una regione,
+    // richiede che
+    // il click sia proprio sopra la stroke, altrimenti cerca la stroke piu'
+    // vicina (max circa 10 pixel)
+    const double maxDist2 = (styleId == 0) ? 100.0 * radius2 : 0;
+    bool strokeFound;
+    double dist2, w, thick;
+    UINT index;
+    //! funzionerebbe ancora meglio con un getNearestStroke che considera
+    // la thickness, cioe' la min distance dalla outline e non dalla centerLine
+    strokeFound = vi->getNearestStroke(pos, w, index, dist2);
+    if (strokeFound) {
+      TStroke *stroke = vi->getStroke(index);
+      thick           = stroke->getThickPoint(w).thick;
+      if (dist2 - thick * thick < maxDist2) {
+        assert(stroke);
+        styleId = stroke->getStyle();
+      }
+    }
+  }
+  return styleId;
+}
+
+//---------------------------------------------------------
+/*--- Toonz Raster LevelのToneを拾う。 ---*/
+int StylePicker::pickTone(const TPointD &pos) {
+  if (TToonzImageP ti = m_image) {
+    TRasterCM32P ras = ti->getRaster();
+    if (!ras) return -1;
+    TPoint point = getRasterPoint(pos);
+    if (!ras->getBounds().contains(point)) return -1;
+    TPixelCM32 col = ras->pixels(point.y)[point.x];
+
+    return col.getTone();
+  } else
+    return -1;
+}
+
+//---------------------------------------------------------
+
+TPixel32 StylePicker::pickColor(const TPointD &pos, double radius2) const {
+  TToonzImageP ti  = m_image;
+  TRasterImageP ri = m_image;
+  if (!!ri)  // !!ti || !!ri)
+  {
+    TRasterP raster;
+    // if(ti)
+    //  raster = ti->getRGBM(true);
+    // else
+    raster = ri->getRaster();
+
+    TPoint point = getRasterPoint(pos);
+    if (!raster->getBounds().contains(point)) return TPixel32::Transparent;
+
+    TRaster32P raster32 = raster;
+    if (raster32) return raster32->pixels(point.y)[point.x];
+
+    TRasterGR8P rasterGR8 = raster;
+    if (rasterGR8) return toPixel32(rasterGR8->pixels(point.y)[point.x]);
+  } else if (TVectorImageP vi = m_image) {
+    const TPalette *palette = m_palette.getPointer();
+    if (!palette) return TPixel32::Transparent;
+    int styleId = pickStyleId(pos, radius2);
+    if (0 <= styleId && styleId < palette->getStyleCount())
+      return palette->getStyle(styleId)->getAverageColor();
+  }
+  return TPixel32::Transparent;
+}
+
+//---------------------------------------------------------
 
 namespace {
 
-//========================================================================
-// Pick Style Tool
-//------------------------------------------------------------------------
-
-class StylePickerTool : public TTool {
-  int m_oldStyleId, m_currentStyleId;
-
-  TEnumProperty m_colorType;
-  TPropertyGroup m_prop;
-
-  TBoolProperty m_passivePick;
-
-public:
-  TPropertyGroup *getProperties(int targetType) override { return &m_prop; }
-
-  StylePickerTool()
-      : TTool("T_StylePicker")
-      , m_currentStyleId(0)
-      , m_colorType("Mode:")
-      , m_passivePick("Passive Pick", false) {
-    m_prop.bind(m_colorType);
-    m_colorType.addValue(AREAS);
-    m_colorType.addValue(LINES);
-    m_colorType.addValue(ALL);
-    m_colorType.setId("Mode");
-    bind(TTool::CommonLevels);
-
-    m_prop.bind(m_passivePick);
-    m_passivePick.setId("PassivePick");
+TPixel32 getAverageColor(const TRect &rect) {
+  GLenum fmt =
+#ifdef TNZ_MACHINE_CHANNEL_ORDER_BGRM
+      GL_RGBA;
+#elif TNZ_MACHINE_CHANNEL_ORDER_MBGR
+      GL_ABGR_EXT;
+#elif TNZ_MACHINE_CHANNEL_ORDER_RGBM
+      GL_RGBA;
+#elif TNZ_MACHINE_CHANNEL_ORDER_MRGB
+      GL_BGRA;
+#else
+//   Error  PLATFORM NOT SUPPORTED
+#error "unknown channel order!"
+#endif
+  UINT r = 0, g = 0, b = 0, m = 0;
+  std::vector<TPixel32> buffer(rect.getLx() * rect.getLy());
+  glReadPixels(rect.x0, rect.y0, rect.getLx(), rect.getLy(), fmt,
+               GL_UNSIGNED_BYTE, &buffer[0]);
+  int size = rect.getLx() * rect.getLy();
+  for (int i = 0; i < size; i++) {
+    r += buffer[i].r;
+    g += buffer[i].g;
+    b += buffer[i].b;
   }
 
-  ToolType getToolType() const override { return TTool::LevelReadTool; }
-  
-  void updateTranslation() {
-    m_colorType.setQStringName(tr("Mode:"));
-    m_passivePick.setQStringName(tr("Passive Pick"));
-  }
+  return TPixel32(b / size, g / size, r / size, 255);
+}
 
-  void draw() override {}
+//---------------------------------------------------------
 
-  void leftButtonDown(const TPointD &pos, const TMouseEvent &e) override {
-    m_oldStyleId = m_currentStyleId =
-        getApplication()->getCurrentLevelStyleIndex();
-    pick(pos, e);
-  }
-  void leftButtonDrag(const TPointD &pos, const TMouseEvent &e) override {
-    pick(pos, e);
-  }
+TPixel32 getAverageColor(TStroke *stroke) {
+  GLenum fmt =
+#ifdef TNZ_MACHINE_CHANNEL_ORDER_BGRM
+      GL_RGBA;
+#elif TNZ_MACHINE_CHANNEL_ORDER_MBGR
+      GL_ABGR_EXT;
+#elif TNZ_MACHINE_CHANNEL_ORDER_RGBM
+      GL_RGBA;
+#elif TNZ_MACHINE_CHANNEL_ORDER_MRGB
+      GL_BGRA;
+#else
+//   Error  PLATFORM NOT SUPPORTED
+#error "unknown channel order"
+#endif
 
-  void pick(const TPointD &pos, const TMouseEvent &e) {
-    // Area = 0, Line = 1, All = 2
-    int modeValue = m_colorType.getIndex();
+  // leggo il buffer e mi prendo i pixels
+  UINT r = 0, g = 0, b = 0, m = 0;
+  TRect rect = convert(stroke->getBBox());
+  std::vector<TPixel32> buffer(rect.getLx() * rect.getLy());
+  glReadPixels(rect.x0, rect.y0, rect.getLx(), rect.getLy(), fmt,
+               GL_UNSIGNED_BYTE, &buffer[0]);
 
-    //------------------------------------
-    // MultiLayerStylePicker
-    /*---
-                    PickしたStyleId = 0、かつ
-                    Preference で MultiLayerStylePickerが有効、かつ
-                    Scene編集モード、かつ
-                    下のカラムから拾った色がTransparentでない場合、
-                    → カレントLevelを移動する。
-    ---*/
-    if (Preferences::instance()->isMultiLayerStylePickerEnabled() &&
-        getApplication()->getCurrentFrame()->isEditingScene()) {
-      int superPickedColumnId = getViewer()->posToColumnIndex(
-          e.m_pos, getPixelSize() * getPixelSize(), false);
+  // calcolo le regioni dello stroke
+  TVectorImage aux;
+  aux.addStroke(stroke);
+  aux.transform(TTranslation(convert(-rect.getP00())));
+  aux.findRegions();
+  int regionCount = aux.getRegionCount();
+  int size = 0, lx = rect.getLx();
 
-      if (superPickedColumnId >= 0 /*-- 何かColumnに当たった場合 --*/
-          &&
-          getApplication()->getCurrentColumn()->getColumnIndex() !=
-              superPickedColumnId) /*-- かつ、Current Columnでない場合 --*/
-      {
-        /*-- そのColumnからPickを試みる --*/
-        int currentFrame = getApplication()->getCurrentFrame()->getFrame();
-        TXshCell pickedCell =
-            getApplication()->getCurrentXsheet()->getXsheet()->getCell(
-                currentFrame, superPickedColumnId);
-        TImageP pickedImage           = pickedCell.getImage(false).getPointer();
-        TToonzImageP picked_ti        = pickedImage;
-        TVectorImageP picked_vi       = pickedImage;
-        TXshSimpleLevel *picked_level = pickedCell.getSimpleLevel();
-        if ((picked_ti || picked_vi) && picked_level) {
-          TPointD tmpMousePosition =
-              getColumnMatrix(superPickedColumnId).inv() *
-              getViewer()->winToWorld(e.m_pos);
-
-          TPointD tmpDpiScale =
-              getCurrentDpiScale(picked_level, getCurrentFid());
-
-          tmpMousePosition.x /= tmpDpiScale.x;
-          tmpMousePosition.y /= tmpDpiScale.y;
-
-          StylePicker superPicker(pickedImage);
-          int picked_subsampling =
-              picked_level->getImageSubsampling(pickedCell.getFrameId());
-          int superPicked_StyleId = superPicker.pickStyleId(
-              TScale(1.0 / picked_subsampling) * tmpMousePosition +
-                  TPointD(-0.5, -0.5),
-              getPixelSize() * getPixelSize(), modeValue);
-          /*-- 何かStyleが拾えて、Transparentでない場合 --*/
-          if (superPicked_StyleId > 0) {
-            /*-- Levelの移動 --*/
-            getApplication()->getCurrentLevel()->setLevel(picked_level);
-            /*-- Columnの移動 --*/
-            getApplication()->getCurrentColumn()->setColumnIndex(
-                superPickedColumnId);
-            /*-- 選択の解除 --*/
-            if (getApplication()->getCurrentSelection()->getSelection())
-              getApplication()
-                  ->getCurrentSelection()
-                  ->getSelection()
-                  ->selectNone();
-            /*-- StyleIdの移動 --*/
-            getApplication()->setCurrentLevelStyleIndex(superPicked_StyleId);
-            return;
-          }
+  for (int j = 0; j < regionCount; j++) {
+    TRegion *reg  = aux.getRegion(j);
+    TRect regRect = convert(reg->getBBox());
+    for (int y = regRect.y0; y < regRect.y1; y++) {
+      std::vector<double> intersections;
+      reg->computeScanlineIntersections(y, intersections);
+      assert(!(intersections.size() & 0x1));
+      for (UINT i = 0; i < intersections.size(); i += 2) {
+        if (intersections[i] == intersections[i + 1]) continue;
+        int firstInters  = (int)intersections[i];
+        int secondInters = (int)intersections[i + 1];
+        for (int x = firstInters + 1; x < secondInters - 1; x++) {
+          r += buffer[y * lx + x].r;
+          g += buffer[y * lx + x].g;
+          b += buffer[y * lx + x].b;
+          size++;
         }
       }
     }
-    /*-- MultiLayerStylePicker ここまで --*/
-    //------------------------------------
-    TImageP image    = getImage(false);
-    TToonzImageP ti  = image;
-    TVectorImageP vi = image;
-    TXshSimpleLevel *level =
-        getApplication()->getCurrentLevel()->getSimpleLevel();
-    if ((!ti && !vi) || !level) return;
-
-    /*-- 画面外をpickしても拾えないようにする --*/
-    if (!m_viewer->getGeometry().contains(pos)) return;
-
-    int subsampling = level->getImageSubsampling(getCurrentFid());
-    StylePicker picker(image);
-    int styleId = picker.pickStyleId(
-        TScale(1.0 / subsampling) * pos + TPointD(-0.5, -0.5),
-        getPixelSize() * getPixelSize(), modeValue);
-
-    if (styleId < 0) return;
-
-    if (modeValue == 1)  // LINES
-    {
-      /*-- pickLineモードのとき、取得Styleが0の場合はカレントStyleを変えない。
-       * --*/
-      if (styleId == 0) return;
-      /*--
-       * pickLineモードのとき、PurePaintの部分をクリックしてもカレントStyleを変えない
-       * --*/
-      if (ti &&
-          picker.pickTone(TScale(1.0 / subsampling) * pos +
-                          TPointD(-0.5, -0.5)) == 255)
-        return;
-    }
-
-    /*--- Styleを選択している場合は選択を解除する ---*/
-    TSelection *selection =
-        TTool::getApplication()->getCurrentSelection()->getSelection();
-    if (selection) {
-      TStyleSelection *styleSelection =
-          dynamic_cast<TStyleSelection *>(selection);
-      if (styleSelection) styleSelection->selectNone();
-    }
-
-    getApplication()->setCurrentLevelStyleIndex(styleId);
   }
 
-  void mouseMove(const TPointD &pos, const TMouseEvent &e) override {
-    if (!m_passivePick.getValue()) return;
-    /*--- PassiveにStyleを拾う機能 ---*/
-    PaletteController *controller =
-        TTool::getApplication()->getPaletteController();
-
-    TImageP image    = getImage(false);
-    TToonzImageP ti  = image;
-    TVectorImageP vi = image;
-    TXshSimpleLevel *level =
-        getApplication()->getCurrentLevel()->getSimpleLevel();
-    if ((!ti && !vi) || !level || !m_viewer->getGeometry().contains(pos)) {
-      controller->notifyStylePassivePicked(-1, -1, -1);
-      return;
-    }
-
-    int subsampling = level->getImageSubsampling(getCurrentFid());
-    StylePicker picker(image);
-    TPointD pickPos(TScale(1.0 / subsampling) * pos + TPointD(-0.5, -0.5));
-    int inkStyleId =
-        picker.pickStyleId(pickPos, getPixelSize() * getPixelSize(), 1);
-    int paintStyleId =
-        picker.pickStyleId(pickPos, getPixelSize() * getPixelSize(), 0);
-    int tone = picker.pickTone(pickPos);
-    controller->notifyStylePassivePicked(inkStyleId, paintStyleId, tone);
-  }
-
-  void onActivate() override {}
-
-  int getCursorId() const override {
-    bool isBlackBG = ToonzCheck::instance()->getChecks() & ToonzCheck::eBlackBg;
-
-    if (m_colorType.getValue() == LINES)
-      return (isBlackBG) ? ToolCursor::PickerCursorWhiteLine
-                         : ToolCursor::PickerCursorLine;
-    else if (m_colorType.getValue() == AREAS)
-      return (isBlackBG) ? ToolCursor::PickerCursorWhiteArea
-                         : ToolCursor::PickerCursorArea;
-    else  // line&areas
-      return (isBlackBG) ? ToolCursor::PickerCursorWhite
-                         : ToolCursor::PickerCursor;
-  }
-
-} stylePickerTool;
+  if (size != 0)
+    return TPixel32(b / size, g / size, r / size, 255);
+  else
+    return TPixel32(buffer[0].b, buffer[0].g, buffer[0].r, 255);
 }
+
+}  // namspace
+
+//---------------------------------------------------------
+
+TPixel32 StylePicker::pickColor(const TRectD &area) const {
+  // TRectD rect=area.enlarge(-1,-1);
+  return getAverageColor(convert(area));
+}
+
+//---------------------------------------------------------
+
+TPixel32 StylePicker::pickColor(TStroke *stroke) const {
+  return getAverageColor(stroke);
+}
+
+//---------------------------------------------------------
