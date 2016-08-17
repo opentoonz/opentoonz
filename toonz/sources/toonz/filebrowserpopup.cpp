fd7dacad04d5e8c19f20c9da3d019aa8fd95effc
Jeremy Bullock <jcbullock@gmail.com>
2016-08-17 07:33:31 -0600

    added animateclick

--- toonz/sources/toonz/filebrowserpopup.cpp
+++ toonz/sources/toonz/filebrowserpopup.cpp
@@ -408,7 +408,7 @@ TFilePath GenericLoadFilePopup::getPath() {
 
 GenericSaveFilePopup::GenericSaveFilePopup(const QString &title)
     : FileBrowserPopup(title) {
-  connect(m_nameField, SIGNAL(returnPressedNow()), this, SLOT(onOkPressed()));
+  connect(m_nameField, SIGNAL(returnPressedNow()), m_okButton, SLOT(animateClick()));
 }
 
 //-----------------------------------------------------------------------------
@@ -548,7 +548,7 @@ void LoadSubScenePopup::showEvent(QShowEvent *e) {
 SaveSceneAsPopup::SaveSceneAsPopup() : FileBrowserPopup(tr("Save Scene")) {
   setOkText(tr("Save"));
   addFilterType("tnz");
-  connect(m_nameField, SIGNAL(returnPressedNow()), this, SLOT(onOkPressed()));
+  connect(m_nameField, SIGNAL(returnPressedNow()), m_okButton, SLOT(animateClick()));
 }
 
 bool SaveSceneAsPopup::execute() {
@@ -578,7 +578,7 @@ void SaveSceneAsPopup::initFolder() {
 SaveSubSceneAsPopup::SaveSubSceneAsPopup()
     : FileBrowserPopup(tr("Sub-xsheet")) {
   setOkText(tr("Save"));
-  connect(m_nameField, SIGNAL(returnPressedNow()), this, SLOT(onOkPressed()));
+  connect(m_nameField, SIGNAL(returnPressedNow()), m_okButton, SLOT(animateClick()));
 }
 
 bool SaveSubSceneAsPopup::execute() {
@@ -1327,7 +1327,7 @@ void LoadLevelPopup::onSelectionChanged(TSelection *selection) {
 
 SaveLevelAsPopup::SaveLevelAsPopup() : FileBrowserPopup(tr("Save Level")) {
   setOkText(tr("Save"));
-  connect(m_nameField, SIGNAL(returnPressedNow()), this, SLOT(onOkPressed()));
+  connect(m_nameField, SIGNAL(returnPressedNow()), m_okButton, SLOT(animateClick()));
 }
 
 bool SaveLevelAsPopup::execute() {
@@ -1608,7 +1608,7 @@ SavePaletteAsPopup::SavePaletteAsPopup()
     : FileBrowserPopup(tr("Save Palette")) {
   setOkText(tr("Save"));
   addFilterType("tpl");
-  connect(m_nameField, SIGNAL(returnPressedNow()), this, SLOT(onOkPressed()));
+  connect(m_nameField, SIGNAL(returnPressedNow()), m_okButton, SLOT(animateClick()));
 }
 
 bool SavePaletteAsPopup::execute() {
