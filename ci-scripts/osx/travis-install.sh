dce3c795d30283c508366087dd3c2280734b44e8
shun_iwasawa <shun_iwasawa@c.dwango.co.jp>
2017-02-16 11:47:59 +0900

    try using older version of clang-format

--- ci-scripts/osx/travis-install.sh
+++ ci-scripts/osx/travis-install.sh
@@ -2,4 +2,8 @@
 brew update
 brew install qt55 glew lz4 lzo libusb
 brew tap tcr/tcr
-brew install clang-format
+# Use older version of clang-format 
+brew tap homebrew/versions
+brew search clang-format
+brew install homebrew/versions/clang-format38
+# brew install clang-format
