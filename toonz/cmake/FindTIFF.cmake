55777afe2dec1db63d89348e965798817fab12d0
Houkime <norgond@excite.co.jp>
2017-11-30 06:51:42 +0000

    Now cmake makes libtiff if it is abscent and on Unixlike.

--- toonz/cmake/FindTIFF.cmake
+++ toonz/cmake/FindTIFF.cmake
@@ -27,6 +27,21 @@ find_library(
     NO_DEFAULT_PATH
 )
 
+if (NOT TIFF_FOUND AND BUILD_ENV_UNIXLIKE)
+    exec_program(./configure ${TIFF_INCLUDE_DIR}/..)
+    exec_program(make ${TIFF_INCLUDE_DIR}/..)
+    find_library(
+        TIFF_LIBRARY
+        NAMES
+            libtiff.a
+        HINTS
+            ${SDKROOT}
+        PATH_SUFFIXES
+            tiff-4.0.3/libtiff/.libs
+        NO_DEFAULT_PATH
+    )
+endif()
+
 message("***** libtiff Header path:" ${TIFF_INCLUDE_DIR})
 message("***** libtiff Library path:" ${TIFF_LIBRARY})
 
