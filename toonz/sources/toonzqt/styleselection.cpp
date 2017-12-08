021ee25bff15c396eeedc4c2d659ab0a906b7ebd
Dave Amies <damies13@gmail.com>
2017-12-08 23:04:05 +1000

    fix logic error
    spotted by @shun-iwasawa, the benifits of peer review :) a luxury I don't normally get.

--- toonz/sources/toonzqt/styleselection.cpp
+++ toonz/sources/toonzqt/styleselection.cpp
@@ -91,7 +91,7 @@ bool pasteStylesDataWithoutUndo(TPalette *palette, TPaletteHandle *pltHandle,
       break;
     }
 
-    if (palette->getStylePage(styleId) != NULL) {
+    if (palette->getStylePage(styleId) == NULL) {
       // styleId non e' utilizzato: uso quello
       // (cut/paste utilizzato per spostare stili)
       palette->setStyle(styleId, style);
