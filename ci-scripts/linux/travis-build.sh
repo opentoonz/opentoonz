44093eea11308fd2805f8eecd498ee8991fb45df
Michał Janiszewski <janisozaur@users.noreply.github.com>
2017-12-21 23:34:12 +0100

    Build using ninja

--- ci-scripts/linux/travis-build.sh
+++ ci-scripts/linux/travis-build.sh
@@ -5,7 +5,5 @@ popd
 cd toonz && mkdir build && cd build
 source /opt/qt59/bin/qt59-env.sh
 cmake ../sources \
-    -DWITH_SYSTEM_SUPERLU:BOOL=OFF
-# according to https://docs.travis-ci.com/user/ci-environment/#Virtualization-environments
-# travis can offer up to 2 cores in burst, try using that
-make -j 2
+    -DWITH_SYSTEM_SUPERLU:BOOL=OFF -G Ninja
+ninja
