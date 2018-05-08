66990bcaf9226b123f6474097e514e74cadb469f
manongjohn <dancel@ptd.net>
2018-05-08 01:53:15 -0400

    Fix inserting column on empty scene

--- toonz/sources/toonz/columncommand.cpp
+++ toonz/sources/toonz/columncommand.cpp
@@ -702,6 +702,10 @@ void InsertEmptyColumnsUndo::redo() const {
   TApp *app    = TApp::instance();
   TXsheet *xsh = app->getCurrentXsheet()->getXsheet();
 
+  // If this is the very first column, add one now since there is always
+  // 1 visible on the screen but its not actually there yet.
+  if (!xsh->getColumnCount()) xsh->insertColumn(0);
+
   std::vector<std::pair<int, int>>::const_reverse_iterator bt,
       bEnd = m_columnBlocks.rend();
   for (bt = m_columnBlocks.rbegin(); bt != bEnd; ++bt)
