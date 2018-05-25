3c8af5e55afd0b6e5ae33ac2b8136f02014a1279
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2018-05-25 22:06:17 +0700

    Typo

--- toonz/sources/translations/update.sh
+++ toonz/sources/translations/update.sh
@@ -5,7 +5,7 @@
 BASE_DIR=$(cd `dirname "$0"`; pwd)
 TOONZLANG=$1
 
-[ ! -d "${BASE_DIR}/${TOONZLANG}" ] || mkdir -p "${BASE_DIR}/${TOONZLANG}"
+[ -d "${BASE_DIR}/${TOONZLANG}" ] || mkdir -p "${BASE_DIR}/${TOONZLANG}"
 cd "${BASE_DIR}/${TOONZLANG}"
 lupdate ../../colorfx/ -ts colorfx.ts
 lupdate ../../common/ -ts tnzcore.ts
