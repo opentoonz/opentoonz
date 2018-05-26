f4608fe9d965a5d7f89ae31f32285cc74a4b243a
Ivan Mahonin <bh@icystar.com>
2018-05-26 13:28:27 +0700

    #common #bug #convert: fix command line convertion of PLI for linux
    Qt documentation for QOffscreenSurface:
    
    Due to the fact that QOffscreenSurface is backed by a QWindow on some
    platforms, cross-platform applications must ensure that create() is only
    called on the main (GUI) thread. The QOffscreenSurface is then safe to
    be used with makeCurrent() on other threads, but the initialization and
    destruction must always happen on the main (GUI) thread.

--- toonz/sources/tconverter/tconverter.cpp
+++ toonz/sources/tconverter/tconverter.cpp
@@ -29,6 +29,10 @@
 #include "tvectorrenderdata.h"
 #include "tofflinegl.h"
 
+#if defined(LINUX)
+#include <QGuiApplication>
+#endif
+
 using namespace std;
 using namespace TCli;
 
@@ -357,6 +361,10 @@ void convert(const TFilePath &source, const TFilePath &dest,
 //------------------------------------------------------------------------
 
 int main(int argc, char *argv[]) {
+#if defined(LINUX)
+  QGuiApplication app(argc, argv);
+#endif
+
   TEnv::setApplication(applicationName, applicationVersion);
   TEnv::setRootVarName(rootVarName);
   TEnv::setSystemVarPrefix(systemVarPrefix);
