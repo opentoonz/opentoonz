efba1528f9d6f8fe5b39f0c6b6115022522270f6
Michał Janiszewski <janisozaur@gmail.com>
2018-05-31 00:48:48 +0200

    Use Ninja for building on Travis

--- ci-scripts/linux/travis-build.sh
+++ ci-scripts/linux/travis-build.sh
@@ -4,11 +4,9 @@ CFLAGS="-fPIC" CXXFLAGS="-fPIC" ./configure --disable-jbig && make
 popd
 cd toonz && mkdir build && cd build
 source /opt/qt59/bin/qt59-env.sh
-cmake ../sources \
+cmake ../sources -G Ninja\
     -DWITH_SYSTEM_SUPERLU:BOOL=OFF
-# according to https://docs.travis-ci.com/user/ci-environment/#Virtualization-environments
-# travis can offer up to 2 cores in burst, try using that
-make -j 2
+ninja
 cd ../../
 ccache --show-stats > ccache_post
 diff -U100 ccache_pre ccache_post # report ccache's efficiency
