708c297f63f95dc09354233a23f443e49d373c3c
Michał Janiszewski <janisozaur@users.noreply.github.com>
2018-05-31 09:17:40 +0200

    Update clang ccache symlinks

--- ci-scripts/linux/travis-install.sh
+++ ci-scripts/linux/travis-install.sh
@@ -9,7 +9,7 @@ wget http://mirrors.kernel.org/ubuntu/pool/main/l/lz4/liblz4-dev_0.0~r131-2ubunt
 sudo dpkg -i liblz4.deb liblz4-dev.deb
 
 # ccache doesn't come with symlink for clang, create them manually
-sudo ln -sf $(which clang) /usr/lib/ccache/clang
-sudo ln -sf $(which clang++) /usr/lib/ccache/clang++
+sudo ln -sf /usr/bin/ccache /usr/lib/ccache/clang
+sudo ln -sf /usr/bin/ccache /usr/lib/ccache/clang++
 
 ccache --show-stats > ccache_pre
