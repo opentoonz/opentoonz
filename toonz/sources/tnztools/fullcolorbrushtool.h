3e24ca61fa935851b966649b7c24be86dc961c2d
JP Parks <74626650+JPUser1@users.noreply.github.com>
2022-06-05 20:05:57 -0600

    Straight Line Tool on Raster Brush Tool
    Straight Line Tool on Raster Brush Tool
    
    Co-Authored-By: jeremybullock <79284068+jeremybullock@users.noreply.github.com>

--- toonz/sources/tnztools/fullcolorbrushtool.h
+++ toonz/sources/tnztools/fullcolorbrushtool.h
@@ -122,7 +122,11 @@ protected:
   bool m_firstTime;
   bool m_mousePressed = false;
   TMouseEvent m_mouseEvent;
-
+  
+  bool m_isStraight = false;
+  TPointD m_firstPoint;
+  TPointD m_lastPoint;
+  
   bool m_propertyUpdating = false;
 };
 
