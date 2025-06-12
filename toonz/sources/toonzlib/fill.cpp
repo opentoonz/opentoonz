eaf5ff5d1b75a3cd039ca6978b7f93c1cbd5ed39
Abel You <abelyou032@gmail.com>
2025-06-12 20:58:29 +0800

    Fix: adjust fill behavior for shift fill option to avoid infinite recursion and stack overflow

--- toonz/sources/toonzlib/fill.cpp
+++ toonz/sources/toonzlib/fill.cpp
@@ -530,7 +530,9 @@ bool fill(const TRasterCM32P &r, const FillParameters &params,
   if (paintAtClickedPos == paint)
     if (params.m_shiftFill) {
       FillParameters tmp = FillParameters(params);
-      tmp.m_styleId = 0;
+      tmp.m_styleId      = 0;
+      tmp.m_shiftFill    = false;
+      tmp.m_minFillDepth = params.m_maxFillDepth;
       fill(r, tmp, saver, Ref);
       paintAtClickedPos = pix0->getPaint();
     } else
