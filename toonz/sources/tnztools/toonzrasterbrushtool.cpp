80cf82bff0bc56aaf408e57b0d376635cef7a28b
JPUser1 <121512756+JParks123@users.noreply.github.com>
2026-02-09 04:21:18 -0700

    Fix controlling Smart Raster/Vector brush resize
    This fixes an issue where tracking of mouse was incorrect when resizing the Raster which made it difficult to control the size of both the min and max sizes.

--- toonz/sources/tnztools/toonzrasterbrushtool.cpp
+++ toonz/sources/tnztools/toonzrasterbrushtool.cpp
@@ -1503,11 +1503,14 @@ void ToonzRasterBrushTool::inputMouseMove(const TPointD &position,
                              m_brushPos + TPointD(radius, radius));
 
   } else {
-    m_brushPos = m_mousePos = position;
+    m_brushPos = getCenteredCursorPos(pos);
 
     invalidateRect += TRectD(position - halfThick, position + halfThick);
   }
 
+  m_mousePos       = pos;
+  m_windowMousePos = -e.m_pos;
+  
   invalidate(invalidateRect.enlarge(20));
 
   if (m_minThick == 0 && m_maxThick == 0) {
