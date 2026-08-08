bad6b8cc1010190a331ac68521b8a6c7e6fb0d2b
Rodney <rodney.baker@gmail.com>
2026-08-08 00:44:27 -0500

    Allow scene saves without icon rendering

--- toonz/sources/include/toonz/toonzscene.h
+++ toonz/sources/include/toonz/toonzscene.h
@@ -100,9 +100,9 @@ public:
   void clear();  //!< Clears the scene.
 
   void save(
-      const TFilePath &path,
-      TXsheet *subxsheet = 0);  //!< Saves the scene (or a sub-xsheet) at the
-                                //!  specified path.
+      const TFilePath &path, TXsheet *subxsheet = 0,
+      bool saveSceneIcon = true);  //!< Saves the scene (or a sub-xsheet) at the
+                                    //!  specified path.
   void loadTnzFile(
       const TFilePath &path);  //!< Loads scene data from file, \a excluding the
                                //!  associated project and the scene resources.
